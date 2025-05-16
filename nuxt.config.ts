e81cc2c436b6fba7f4974270caa0bd0d3f745ae5
fire8327 <fire83274@gmail.com>
2025-05-16 23:07:01 +0300

    create project

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/icon',
+    '@formkit/nuxt',
+    '@nuxtjs/supabase',
+    '@pinia/nuxt'
+  ],
+  postcss: {
+    plugins: {
+      cssnano: false 
+    }
+  },
+
+  supabase: {
+    redirect: false
+  }
+})
