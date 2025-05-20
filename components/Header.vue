fbe167ce02290bfa79af61358e20d80908ee78b3
Леонид <114702513+fire8327@users.noreply.github.com>
2025-05-20 19:19:52 +0000

    upd header

--- components/Header.vue
+++ components/Header.vue
@@ -5,7 +5,7 @@
                 <Icon class="text-3xl text-amber-500" name="ph:books-duotone"/>
                 <span class="font-medium font-mono">Library</span>
             </NuxtLink>
-            <div class="flex items-center gap-6">
+            <div class="flex items-center gap-6 max-lg:flex-col max-lg:fixed max-lg:py-6 max-lg:w-full max-lg:bg-white max-lg:z-[6] max-lg:left-0 max-lg:rounded-t-xl transition-all duration-500" :class="isMenuShow ? 'max-lg:bottom-0' : 'max-lg:top-full max-lg:translate-y-full'">
                 <NuxtLink to="/" class="flex flex-col after:w-0 after:h-px after:bg-amber-500 after:transition-all after:duration-500 hover:after:w-full">Главная</NuxtLink>
                 <NuxtLink to="/catalog" class="flex flex-col after:w-0 after:h-px after:bg-amber-500 after:transition-all after:duration-500 hover:after:w-full">Каталог</NuxtLink>
                 <NuxtLink to="/about" class="flex flex-col after:w-0 after:h-px after:bg-amber-500 after:transition-all after:duration-500 hover:after:w-full">О нас</NuxtLink>
@@ -14,7 +14,11 @@
                     <Icon class="text-3xl text-amber-500" name="material-symbols:person"/>
                 </NuxtLink>
             </div>
+            <button class="lg:hidden flex" @click="isMenuShow = !isMenuShow">
+                <Icon class="text-3xl text-amber-500" name="ph:list-bold"/>         
+            </button>
         </div>
+        <div @click="isMenuShow = false" :class="{'max-lg:hidden' : !isMenuShow}" class="inset-0 fixed bg-black/30 lg:hidden max-lg:z-[5]"></div>
         <button type="button" @click="messageTitle = null" class="fixed top-10 right-10 z-[11] cursor-pointer flex items-center gap-2 px-6 py-2 text-lg rounded-xl w-fit font-medium font-mono bg-white border border-[#131313]/10 shadow-[0px_0px_13px_-7px_#131313]" :class="messageType ? ' text-[#131313]/80' : 'text-red-500'" v-if="messageTitle">
             <Icon class="text-3xl" name="material-symbols:close-small-rounded"/>
             <span>{{messageTitle}}</span>
@@ -23,6 +27,17 @@
 </template>
 
 <script setup>
+/* мобильное меню */
+const isMenuShow = ref(false)
+
+
+/* закрытие мобильного меню */
+const nuxtApp = useNuxtApp()
+nuxtApp.hook('page:start', () => {
+    isMenuShow.value = false
+})
+
+
 /* создание сообщений */
 const { messageTitle, messageType } = storeToRefs(useMessagesStore())
 </script>
